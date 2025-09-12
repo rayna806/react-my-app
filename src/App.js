0012a3ee3e95f7aae8925056a5034759791b4df0
rayna806 <LUORA2@OOCL.COM>
2025-09-12 14:40:17 +0800

    Commit:recreate project

--- src/App.js
+++ src/App.js
@@ -1,54 +1,11 @@
-import './App.css';
-import {createContext, useContext, useReducer, useState} from "react";
-import {Counter} from "./counter/Counter";
-import {CounterGroupGenerator} from "./counter/CounterGroupGenerator";
-import {CounterGroup} from "./counter/CounterGroup";
-// export const counterContext=createContext();
-
-
-export const CounterContext = createContext();
-export const initState = {
-    count: 4
-};
-
-function MultipleCounter() {
-    const {state,dispatch} = useContext(CounterContext)
-    // const [counterSize, setCounterSize] = useState(state.count);
-    return <div>
-        Answer 8 exercise-MultipleCounter
-        <CounterGroupGenerator counterSize = {state.count}/>
-        <CounterGroup counterSize={state.count}/>
-
-    </div>
-}
-
-
-function counterReducer(state, action) {
-
-    // console.log(statr,action)
-    switch (action.type){
-        case "UPDATE_SIZE":
-            return{count: action.payload.number}
-        case "RESET":
-            return {count:0};
-        default:
-            return state;
-    }
-
-}
+export const initState = [
+    {id: 1, text: "the first todo", done: false},
+    {id: 2, text: "the second todo", done: false},
+];
 
 function App() {
-
-    const [state, dispatch ]= useReducer(counterReducer,initState);
     return (
-        <div className="App">
-            {/*<Counter />*/}
-            <CounterContext.Provider value={{
-                state:state,
-                dispatch:dispatch}}>
-                <MultipleCounter />
-            </CounterContext.Provider>
-        </div>
+        <div />
     );
 }
 
