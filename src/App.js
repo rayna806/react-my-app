c46502c55b2c8dd58fe312f745480687f5a79d8b
rayna806 <LUORA2@OOCL.COM>
2025-09-12 14:43:02 +0800

    Commit:init project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,27 @@
+import {createContext, useContext} from "react";
+
 export const initState = [
     {id: 1, text: "the first todo", done: false},
     {id: 2, text: "the second todo", done: false},
 ];
+export const TodoContext = createContext()
+
+function TodoGroup() {
+    return null;
+}
+
+export function todoReducer(state, action) {
+    return state
+}
 
 function App() {
+    const [state, dispatch] = useContext(todoReducer, initState);
     return (
-        <div />
+        <div>
+            <TodoContext.Provider value={{state, dispatch}}>
+                <TodoGroup/>
+            </TodoContext.Provider>
+        </div>
     );
 }
 
