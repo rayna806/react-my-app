7b5944ae27e8b476255bc206bd62a792cfb81f41
rayna806 <LUORA2@OOCL.COM>
2025-09-15 10:53:53 +0800

    Commit: Homepages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import { useReducer } from "react";
+import {useReducer} from "react";
 import './App.css';
-import TodoList from "./components/TodoList";
-import { initialState, todoReducer } from "./reducers/todoReducer";
-import { TodoContext } from "./context/TodoContext";
-import {createBrowserRouter, NavLink, Outlet, RouterProvider,useRouteError} from "react-router";
+import {initialState, todoReducer} from "./reducers/todoReducer";
+import {TodoContext} from "./context/TodoContext";
+import {createBrowserRouter, NavLink, Outlet, RouterProvider} from "react-router";
+import {ErrorPage} from "./pages/ErrorPage";
+import {HomePage} from "./pages/HomePage";
 
 function DefaultLayout() {
     return <div>
@@ -19,17 +20,6 @@ function DefaultLayout() {
         </main>
     </div>
 }
-function ErrorPage() {
-    const error = useRouteError();
-    return <div>
-        {error.status ===404
-            ?<div><h1>404 not found</h1><span>Try</span></div>
-            :<div>{JSON.stringify(error)}</div>}
-
-    </div>;
-}
-
-
 
 const routes= createBrowserRouter([
     {
@@ -39,7 +29,7 @@ const routes= createBrowserRouter([
         children: [
             {
                 path: "/",
-                element:<TodoList />
+                element:<HomePage />
             }
         ]
     }
